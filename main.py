eb90cb3bd04feaf93997205cc65d6c84a769bfac
Viktor Ingemarsson <lars.viktor.ingemarsson@gmail.com>
2019-11-27 09:28:20 +0100

    KIKA MARCUS

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+from ray_optics import refraction
 
 def createImage():
-    print("This line will be printed.")
+    #print("This line will be printed.")
     return 3
 
-def displayImage():
-    print("Should print image")
+#def displayImage():
+    #print("Should print image")
 
-a = createImage();
+a = createImage()
+
+#def refraction(xDim, yDim, dropletRadius, posX, posY, posZ, distZ, rayDensity, refractiveIndexMedium1,
 
 
-print(str(a))
-
-displayImage()
+refraction(2,2,2,0,0,0,10,1,1,1.2)
