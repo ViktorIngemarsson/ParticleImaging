24f018b7706a2837a3a9658038e9d78b01cc475d
Viktor Ingemarsson <lars.viktor.ingemarsson@gmail.com>
2019-12-10 14:58:30 +0100

    conflict

--- main.py
+++ main.py
@@ -22,8 +22,18 @@ p2 = Point(1, 0, 0)
 p3 = Point(0, 1, 0)
 pl = Plane(p1, p2, p3)
 
+pl.disp()
+
+pl.xrotation(0.2).disp()
+
 p3 = Point(3, 1, 7)
 
 kl = pl.translate(p3)
 
 kl.disp()
+print('jk')
+pl.disp()
+k = pl.xrotation(0.2)
+l = k.yrotation(0.22)
+o = l.zrotation(0.67)
+
